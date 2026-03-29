5ef3f981c3a76ec76171d10ca6f5e0302f896d83
Amanda Neves <amandanevoli@gmail.com>
2026-03-29 17:04:34 -0300

    adicionando exercício 4

---
+++ amanda-analivia/e01-php/ex04/index.php
@@ -0,0 +1,32 @@
+<!--
+Data: 30/03/2026
+Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Objetivo: Utilizar estruturas de repetição para gerar padrões numéricos em forma de sequência crescente.
+
+Exercício 4 - Triângulo Numérico
+Leia um número n e imprima n linhas no seguinte formato (exemplo para n = 6):
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Triângulo Numérico</title>
+</head>
+<body>
+<h1>Triângulo Numérico</h1>
+<form action="calcula.php" method="post">
+    <label>Digite um número (n):</label>
+    <input type="number" name="n" min="1" required><br><br>
+
+    <button type="submit">Gerar</button>
+</form>
+
+</body>
+</html>
