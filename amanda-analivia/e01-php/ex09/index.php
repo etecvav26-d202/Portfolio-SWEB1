d10ecc2e0b30b0c9dcfc4dd5b691a0ba967bbc2c
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-03-29 17:16:27 -0300

    adicionando exercício 9

---
+++ amanda-analivia/e01-php/ex09/index.php
@@ -0,0 +1,27 @@
+<!--
+Data: 30/03/2026
+Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Objetivo: Desenvolver função que processa um conjunto de valores e retorna a média.
+
+Exercício 9 - Média Aritmética com Função
+Crie uma função:
+- function media($v)
+Que receba uma lista de números reais e retorne a média aritmética.
+--> 
+<!DOCTYPE html>
+<html lang="pt-br"> 
+    <head>
+        <meta charset="UTF-8">
+        <title> Exercício 9 - Média Aritmética com Função</title>
+</head>
+<body>
+    <h1> Média Aritmética <h1>
+        <form action="calcula.php" method="POST">
+            <label> Faça uma lista de números reais: </label>
+            <input type="text" name="numeros" required size="30"><br><br>
+
+    <button type="submit">Calcular Média</button>
+</form>
+
+</body>
+</html>
