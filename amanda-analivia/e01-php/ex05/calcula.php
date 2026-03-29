76cef473264f190c434e6d4231f1ad2b28244385
Amanda Neves <amandanevoli@gmail.com>
2026-03-29 17:05:04 -0300

    adicionando exercício 5

---
+++ amanda-analivia/e01-php/ex05/calcula.php
@@ -0,0 +1,30 @@
+<?php
+
+
+function fatorial($num) {
+    $fat = 1;
+
+    for ($i = 1; $i <= $num; $i++) {
+        $fat *= $i;
+    }
+
+    return $fat;
+}
+
+$n1 = $_POST["n1"];
+$n2 = $_POST["n2"];
+$n3 = $_POST["n3"];
+$n4 = $_POST["n4"];
+$n5 = $_POST["n5"];
+
+$f1 = fatorial($n1);
+$f2 = fatorial($n2);
+$f3 = fatorial($n3);
+$f4 = fatorial($n4);
+$f5 = fatorial($n5);
+
+$soma = $f1 + $f2 + $f3 + $f4 + $f5;
+
+echo "A soma dos fatoriais é: " . $soma;
+echo '<br><a href="index.php">Voltar</a>';
+?>
---
+++ amanda-analivia/e01-php/ex05/index.php
@@ -0,0 +1,36 @@
+<!--
+Data: 30/03/2026
+Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Objetivo: Calcular fatoriais e somar seus resultados, praticando laços de repetição e lógica matemática.
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoresiais desses números.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercício 5 - Somatório de Fatoriais</title>
+</head>
+<body>
+    <h1>Somatório de Fatoriais<h1>
+        <form action="calcula.php" method="POST">
+            <label>Digite 5 números: </label>
+            <br><br>
+            <label> Número 1: </label> <input typer="number" name="n1" required>
+
+            <label> Número 2: </label> <input typer="number" name="n2" required>
+
+            <label> Número 3: </label> <input typer="number" name="n3" required>
+
+            <label> Número 4: </label> <input typer="number" name="n4" required>
+            
+            <label> Número 5: </label> <input typer="number" name="n5" required>
+            <br><br>
+
+            <button type="submit">Calcular</button>
+            
+</form>
+
+</body>
+</html>
