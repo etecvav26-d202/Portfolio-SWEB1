1c2f5c8876d2804788a02b8d0225d32a33ff9077
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-03-29 17:12:09 -0300

    adicionando exercício 6

---
+++ amanda-analivia/e01-php/ex06/calcula.php
@@ -0,0 +1,23 @@
+<?php
+
+$n = $_POST["n"];
+
+$a = 0;
+$b = 1;
+
+echo "Sequência de Fibonacci:<br><br>";
+
+if ($n <= 0) {
+    echo "Digite um número maior que 0.";
+} else {
+
+    for ($i = 1; $i <= $n; $i++) {
+        echo $a . " ";
+
+        $prox = $a + $b; 
+        $a = $b;  
+        $b = $prox;
+    }
+}
+echo '<br><a href="index.php">Voltar</a>';
+?>
---
+++ amanda-analivia/e01-php/ex06/index.php
@@ -0,0 +1,30 @@
+<!--
+Data: 30/03/2026
+Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Objetivo: Gerar uma sequência numérica baseada em regra recursiva/iterativa, utilizando repetição.
+
+Exercício 6 - Série de Fibonacci
+Leia um número ne mostre os primeiros termos da sequência de Fibonacci.
+
+Exemplo:
+n = 12
+Resultado:
+0 1 1 2 3 5 8 13 21 34 55 89: 
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercício 6 - Série de Fibonacci</title>
+</head>
+<body>
+    <h1>Série de Fibonacci<h1> 
+        <form action="calcula.php" method="POST">
+            <label> Digite a quantidade de termos: </label>
+            <input type="number" name="n" required>
+             <br><br>
+             <button type="submit">Mostrar sequência</button>
+</form>
+
+</body>
+</html>
