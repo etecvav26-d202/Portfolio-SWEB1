d523925e9fe7ff7b5da01a17693b6a7fea576a30
Amanda Neves <amandanevoli@gmail.com>
2026-03-29 17:01:51 -0300

    adicionando exercício 1

---
+++ amanda-analivia/e01-php/ex01/calcula.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['numero'])) {
+
+    $numero = $_POST['numero'];
+
+    if ($numero >= 100 && $numero <= 200) {
+        echo "O número está entre 100 e 200.";
+    } else {
+        echo "O número não está entre 100 e 200.";
+    }
+
+} else {
+    echo "Nenhum número foi informado.";
+}
+echo '<br><a href="index.php">Voltar</a>';
+
+?>
---
+++ amanda-analivia/e01-php/ex01/index.php
@@ -0,0 +1,25 @@
+<!--
+Data: 30/03/2026
+Autor: Amanda Neves Oliveira e Ana Lívia Takeyama Romanato - 2°D
+Objetivo: Verificar se um número informado pertence a um intervalo específico usando estrutura condicional.
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um número e diga se este número está no intervalo entre 100 e 200.
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 1 - Intervalo entre 100 e 200</title>
+</head>
+<body>
+<h1>Intervalo entre 100 e 200<h1>
+<form action="calcula.php" method="post">
+   <label>Digite um número: </label><br>
+    <input type="number" name="numero" required>
+    <br><br>
+    <button type="submit">Verificar</button>
+</form>
+
+</body>
+</html>
