47127db57ded7e2a782d1399f88b727b335672cd
Isabelli Dias <ddsisabellid@gmail.com>
2026-03-30 11:28:35 -0300

    adicionando arquivo 5

---
+++ aliceg-alicer-isabelli/ex05/index.php
@@ -0,0 +1,33 @@
+<!--
+Data: 30/03/2026
+Autor: Alice Rasmussen Rezende Alves, Alice Gimenez Siqueira e Isabelli Dias da Silva 2°D
+Objetivo: 
+
+Exercício 5 - Somatório de Fatoriais
+Leia 5 números inteiros e mostre a soma dos fatoresiais desses números.
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Exercício 5</title>
+</head>
+<body>
+    <h1> Somatório de Fatoriais <h1>
+        <form action="calcula.php" method="POST">
+            <label> Digite 5 numeros: </label>
+            <br><br>
+            <label> Número 1: </label> <input typer="number" name="n1" required>
+            <label> Número 2: </label> <input typer="number" name="n2" required>
+            <label> Número 3: </label> <input typer="number" name="n3" required>
+            <label> Número 4: </label> <input typer="number" name="n4" required>
+            <label> Número 5: </label> <input typer="number" name="n5" required>
+            <br><br>
+
+            <button type="submit">Calcular</button>
+            
+</form>
+
+</body>
+</html>
