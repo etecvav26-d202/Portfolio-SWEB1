2450b4a0286835eb0a83ad767c9f7f566cb10b3f
Alice Gimenez <alicegimenezsiq@gmail.com>
2026-03-30 11:30:28 -0300

    adicionando arquivo 8

---
+++ aliceg-alicer-isabelli/ex08/index.php
@@ -0,0 +1,26 @@
+<!--
+Data: 30/03/2026
+Autor: Alice Rasmussen Rezende Alves, Alice Gimeniz Siqueira e Isabelli Dias da Silva 2°D
+Objetivo:
+
+Soma de 0 até N com Função
+Crie uma função soma(n)que receba um número inteiro e retorne a soma de 0 até esse número.
+-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title> Exercício 8 </title>
+</head>
+<body>
+    <h1> Soma de 0 até N <h1>
+        <form action="calcula.php" method "POST">
+            <label> Digite um número inteiro: </label>
+            <input type="number" name="n" required>
+
+            <button type="submit"> Mostrar soma </button>
+
+</form>
+</body>
+</html>
