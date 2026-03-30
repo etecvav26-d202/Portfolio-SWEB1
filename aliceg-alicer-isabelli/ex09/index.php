3f02fd517f010746d7a16ab33a1d90c70e68a038
Alice Gimenez <alicegimenezsiq@gmail.com>
2026-03-30 11:30:57 -0300

    adicionando arquivo 9

---
+++ aliceg-alicer-isabelli/ex09/index.php
@@ -0,0 +1,23 @@
+<!--
+Data: 30/03/2026
+Autor: Alice Rasmussen Rezende Alves, Alice Gimenez Siqueira e Isabelli Dias da Silva 2°D
+Objetivo:
+
+Média Aritmética com Função
+Crie uma função:
+
+função mídia($v)
+
+Que receba uma lista de números reais e retorne à mídia aritmética.
+--> 
+
+<!DOCTYPE html>
+<html lang="pt-br"> 
+    <head>
+        <meta charset="UTF-8">
+        <title> Exercício 9 </title>
+</head>
+<body>
+    <h1> Média Aritmética <h1>
+        <form action="" method="POST">
+            <label> Faça uma lista de números reais: </label>
