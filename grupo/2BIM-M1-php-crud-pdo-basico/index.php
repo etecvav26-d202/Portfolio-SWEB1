546989cd15521c67efdaa81cbc3b8e71b63606bd
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-05-17 19:21:01 -0300

    inserindo tabela

--- grupo/2BIM-M1-php-crud-pdo-basico/index.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/index.php
@@ -8,10 +8,21 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute();
 
 $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+?>
 
 <?php
 require 'includes/footer.php';
 ?>
+
+<h2>Lista de Produtos</h2>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Fabricante</th>
+        <th>Preço</th>
+        <th>Estoque</th>
+    </tr>
 
 
