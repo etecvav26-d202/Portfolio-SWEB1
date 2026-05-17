2fac79d6776a4d2ba0e45ecfa8a167dc1f9fc981
Isabelli Dias <ddsisabellid@gmail.com>
2026-05-17 20:10:27 -0300

    inserindo código de buscar os dados do produto

--- grupo/2BIM-M1-php-crud-pdo-basico/editar.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/editar.php
@@ -1,3 +1,13 @@
 <?php
 require_once 'config/conexao.php';
 
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM produtos WHERE id = :id";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    ':id' => $id
+]);
+
