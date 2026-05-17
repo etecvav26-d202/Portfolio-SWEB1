fc3d1a4efe668408a18aa0344198d8a93f38cf80
Amanda Neves <amandanevoli@gmail.com>
2026-05-17 17:15:37 -0300

    adicionando código para verificação de conexão - dada como realizada

--- grupo/2BIM-M1-php-crud-pdo-basico/editar.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/editar.php
@@ -5,3 +5,12 @@ $banco = "farmavav";
 $usuario = "root";
 $senha = "";
 
+try {
+
+    $conexao = new PDO(
+        "mysql:host=$servidor;dbname=$banco;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    echo "Conexão realizada com sucesso!";
