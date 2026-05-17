7d710ec400fb89369c9d7c4e34f79fcc94e68de9
Amanda Neves <amandanevoli@gmail.com>
2026-05-17 17:24:07 -0300

    copiando e colando o código do editar.php para conexao.ph (foi realizada no local incorreto

---
+++ grupo/2BIM-M1-php-crud-pdo-basico/config/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+$servidor = "localhost";
+$banco = "farmavav";
+$usuario = "root";
+$senha = "";
+
+try {
+
+    $conexao = new PDO(
+        "mysql:host=$servidor;dbname=$banco;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    echo "Conexão realizada com sucesso!";
+
+} catch(PDOException $erro){
+    echo "Erro na conexão do servidor: " . $erro->getMessage();
+}
+
+?>
