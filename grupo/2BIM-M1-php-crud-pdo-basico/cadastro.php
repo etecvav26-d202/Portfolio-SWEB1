b497cf9ab1dacd06d3bfda28c90e3ee9bcc2adbe
Alice Gimenez <alicegimenezsiq@gmail.com>
2026-05-17 18:13:33 -0300

    inserindo código que recebe os dados digitados no formulário

--- grupo/2BIM-M1-php-crud-pdo-basico/cadastro.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/cadastro.php
@@ -1,3 +1,11 @@
 <?php
 require_once 'config/conexao.php'
 
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+
+}
