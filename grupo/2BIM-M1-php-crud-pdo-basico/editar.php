3cdad000f5f812c954208455008d33d3fffa6154
Isabelli Dias <ddsisabellid@gmail.com>
2026-05-17 20:15:15 -0300

    inserindo código para novos dados do estoque

--- grupo/2BIM-M1-php-crud-pdo-basico/editar.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/editar.php
@@ -17,5 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nome = $_POST['nome'];
     $fabricante = $_POST['fabricante'];
     $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
     
 }
