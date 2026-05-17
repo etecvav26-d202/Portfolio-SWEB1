f129f2a91119ec54cd2132e3620f5e9541bda279
Isabelli Dias <ddsisabellid@gmail.com>
2026-05-17 20:06:35 -0300

    adicionando variável

--- grupo/2BIM-M1-php-crud-pdo-basico/editar.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/editar.php
@@ -1,22 +1,3 @@
 <?php
+require_once 'config/conexao.php';
 
-$servidor = "localhost";
-$banco = "farmavav";
-$usuario = "root";
-$senha = "";
-
-try {
-
-    $conexao = new PDO(
-        "mysql:host=$servidor;dbname=$banco;charset=utf8",
-        $usuario,
-        $senha
-    );
-
-    echo "Conexão realizada com sucesso!";
-
-} catch(PDOException $erro){
-    echo "Erro na conexão do servidor: " . $erro->getMessage();
-}
-
-?>
