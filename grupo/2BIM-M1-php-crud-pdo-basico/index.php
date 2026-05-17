2140afc7b62fc42ff8a69514655baa22bf7f9d14
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-05-17 18:00:27 -0300

    fazendo correções

--- grupo/2BIM-M1-php-crud-pdo-basico/index.php
+++ grupo/2BIM-M1-php-crud-pdo-basico/index.php
@@ -1,12 +1,12 @@
 <?php
-require_once 'config/conexao.php';
-require_once 'includes/header.php';
+require 'config/conexao.php';
+require 'includes/header.php';
 ?>
 
 <h2>testando</h2>
 
 <?php
-require_once 'includes/footer.php';
+require 'includes/footer.php';
 ?>
 
 
