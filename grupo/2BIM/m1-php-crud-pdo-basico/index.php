3f040600dcb558bb9e3b7f752165f64724130387
Ana Lívia <analiviatakeyama2009@gmail.com>
2026-05-18 11:50:43 -0300

    excluindo tabela e adicionando cards

--- grupo/2BIM/m1-php-crud-pdo-basico/index.php
+++ grupo/2BIM/m1-php-crud-pdo-basico/index.php
@@ -16,69 +16,8 @@ require 'includes/header.php';
 
 <h2>Lista de Produtos</h2>
 
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Fabricante</th>
-        <th>Preço</th>
-        <th>Estoque</th>
-        <th>Ações</th>
-    </tr>
-
-    <?php
-    foreach($produtos as $produto){
-        ?>
-
-        <tr>
-            <td>
-                <?php 
-                echo $produto['id'];
-                ?>
-            </td>
-
-            <td>
-                <?php
-                echo $produto['nome'];
-                ?>
-            </td>
-
-            <td>
-                <?php
-                echo $produto['fabricante'];
-                ?>
-            </td>
-
-            <td>
-                R$ <?php
-                echo $produto['preco'];
-                ?>
-            </td>
-
-            <td>
-                <?php
-                echo $produto['estoque'];
-                ?>
-            </td>
-
-            <td>
-                <a href="editar.php?id=<?php echo $produto['id']; ?>">
-                Editar
-                </a>
-
-                <a href="excluir.php?id=<?php echo $produto['id']; ?>"
-                onclick="return confirm('Tem certeza que deseja excluir o produto?')">
-                Excluir
-                </a>
-            </td>
-
-
-        </tr>
-
-        <?php }
-        ?>
-</table>
-
+<div class="cards-container">
+    
 <?php
 require_once 'includes/footer.php';
 ?>
