9b8093497a4ffbef70e4464ca67b69a9f92225f7
Amanda Neves <amandanevoli@gmail.com>
2026-08-10 08:36:55 -0300

    correções

--- grupo/3BIM/m1-criptografia-php/includes/footer.php
+++ grupo/3BIM/m1-criptografia-php/includes/footer.php
@@ -4,7 +4,7 @@
 </main>
 <footer class="rodape">
     <p>
-        Alice Gimenez, Alice Rasmussen, Amanda Neves e Ana Lívia Takeyama &mdash;
+        Alice Gimenez, Alice Rasmussen, Amanda Neves, Ana Lívia Takeyama e Isabelli Dias &mdash;
         Informática para Internet &mdash;
         <?= date('Y') ?>
     </p>
