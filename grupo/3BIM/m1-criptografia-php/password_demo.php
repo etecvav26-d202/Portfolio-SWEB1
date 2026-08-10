a78053092c5aa781f81433f03b8da233ac8c0831
Amanda Neves <amandanevoli@gmail.com>
2026-08-10 00:07:37 -0300

    inserindo comando para gerar os hashes

--- grupo/3BIM/m1-criptografia-php/password_demo.php
+++ grupo/3BIM/m1-criptografia-php/password_demo.php
@@ -13,3 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $senhaConferir = $_POST['senha_conferir'];
     }
 }
+
+$hashBcrypt = password_hash($senha, PASSWORD_BCRYPT);
+$hashArgon2id = defined('PASSWORD_ARGON2ID')
+    ? password_hash($senha, PASSWORD_ARGON2ID)
+    : null;
+
+// Se o usuário preencheu o campo de conferência, valida contra o hash bcrypt gerado.
+if ($senhaConferir !== '') {
+    $resultadoConferencia = password_verify($senhaConferir, $hashBcrypt);
+}
+
+require __DIR__ . 'includes/header.php';
+?>
