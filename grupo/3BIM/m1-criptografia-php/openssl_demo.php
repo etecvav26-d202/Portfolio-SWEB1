b16c68ca6494e5b312d44633db32c23263116411
Amanda Neves <amandanevoli@gmail.com>
2026-08-10 00:02:52 -0300

    inserindo cifra no texto

--- grupo/3BIM/m1-criptografia-php/openssl_demo.php
+++ grupo/3BIM/m1-criptografia-php/openssl_demo.php
@@ -13,3 +13,5 @@ $chave = hash('sha256', $senha, true); // 32 bytes, exigido pelo AES-256
 
 $tamanhoIv = openssl_cipher_iv_length($metodo);
 $iv = openssl_random_pseudo_bytes($tamanhoIv);
+
+$textoCifrado = openssl_encrypt($texto, $metodo, $chave, OPENSSL_RAW_DATA, $iv);
